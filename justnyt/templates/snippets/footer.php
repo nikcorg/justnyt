2823a654c155983bce4311a565a8b0a6a47d8972
Niklas Lindgren <niklas@sc5.io>
2014-07-06 16:56:35 +0300

    Rearrange footer nav
    Adds tagline

--- justnyt/templates/snippets/footer.php
+++ justnyt/templates/snippets/footer.php
@@ -1,15 +1,15 @@
 <footer>
-    <div class="column left">
+    <div class="column c50 left">
         <nav>
             <ul>
-                <li><a href="/kuraattoriksi">Kuraattoriksi kuraattorin paikalle</a></li>
-                <li><a href="/faq">Kysymyksiä ja vastauksia</a></li>
-                <li><a href="/kuraattorit">Kuraattorit</a></li>
                 <li><a href="/historiaa">Parhauden historiaa</a></li>
+                <li><a href="/kuraattorit">Kuraattorit</a></li>
+                <li><a href="/faq">Kysymyksiä ja vastauksia</a></li>
+                <li><a href="/kuraattoriksi">Kuraattoriksi kuraattorin paikalle</a></li>
             </ul>
         </nav>
     </div>
-    <div class="column right">
-        <p>Blablabla</p>
+    <div class="column c50 left">
+        <p>Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone.</p>
     </div>
 </footer>
