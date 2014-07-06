4d5365d6f79f3f7204c9db0fbbcf361c614b112c
Niklas Lindgren <niklas@sc5.io>
2014-07-06 17:00:23 +0300

    Add RedirectController (initial commit)

--- justnyt/controllers/RedirectController.php
+++ justnyt/controllers/RedirectController.php
@@ -3,7 +3,19 @@ namespace justnyt\controllers;
 
 class RedirectController extends \glue\Controller
 {
+    protected function redirectTo($url) {
+        throw new \glue\exceptions\http\E303Exception($url);
+    }
+
     public function redirect() {
-        throw new \glue\exceptions\http\E303Exception("http://kakspistenolla.com");
+        $newestRecommendation = \justnyt\models\RecommendationQuery::create()
+            ->orderByCreated(\Propel\Runtime\ActiveQuery\Criteria::DESC)
+            ->findOne();
+
+        if (! $newestRecommendation) {
+            throw new \glue\exceptions\http\E404Exception();
+        }
+
+        $this->redirectTo($newestRecommendation->getUrl());
     }
 }
