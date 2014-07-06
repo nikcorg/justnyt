c9df5313538b9cdeaacfaad764490a16fd748cea
Niklas Lindgren <niklas@sc5.io>
2014-07-06 16:59:35 +0300

    Add open graph tags for title and description

--- justnyt/templates/layout.php
+++ justnyt/templates/layout.php
@@ -5,6 +5,10 @@
     <link rel="stylesheet" href="/assets/css/style.css" type="text/css">
     <link rel="shortcut icon" href="/assets/img/glyphicons_054_clock.png" type="image/png">
     <title><?= isset($title) ? $title . " - " : "" ?>Just Nyt!</title>
+
+    <meta property="og:title" content="<?= isset($title) ? $title . " - " : "" ?>Just Nyt!">
+    <meta property="og:description"
+        content="Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone">
 </head>
 <body>
     <?php $this->snippet("snippets/header") ?>
