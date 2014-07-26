2979cf6e30fb46e54067cf0fbcf9bf531d973758
Niklas Lindgren <niklas@sc5.io>
2014-07-26 17:19:47 +0300

    Add some text to index

--- justnyt/templates/index/index.php
+++ justnyt/templates/index/index.php
@@ -1,1 +1,5 @@
-Index
+<p>Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone.</p>
+
+<h2>Miten se toimii?</h2>
+
+<p>Tarvitset vain yhden kirjanmerkin selaimeesi: <a href="/parhautta">justnyt.fi/parhautta</a>. Se ohjaa sinut aina viimeisimpään suositukseen. Se, kuinka usein sisältö päivittyy, riippuu täysin kuraattorista.</p>
