6cadea60b27a50c6efe90e22f77dafd7a8c8bb43
Niklas Lindgren <niklas@sc5.io>
2014-07-26 17:19:13 +0300

    Add form for candidate submission

---
+++ justnyt/templates/index/recommend.php
@@ -0,0 +1,9 @@
+<p>Jos olet vapaaehtoinen kuraattoriksi, jätä sähköpostiosoitteesi alla olevaan lomakkeeseen ja odota palveluunkutsua.</p>
+
+<form action="" method="post">
+    <p>
+        <label><span>Sähköpostiosoitteesi</span>
+        <input type="email" name="email" placeholder="syötä sähköpostiosoitteesi tähän" required></label></p>
+
+    <p><button type="submit">Ilmoittaudun vapaaehtoiseksi</button></p>
+</form>
