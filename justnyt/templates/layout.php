2b46673a6b652bed25246695e2125a5f39aeeee7
Niklas Lindgren <niklas@sc5.io>
2014-07-26 17:01:00 +0300

    Smart title and description in template

--- justnyt/templates/layout.php
+++ justnyt/templates/layout.php
@@ -4,11 +4,18 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="/assets/css/style.css" type="text/css">
     <link rel="shortcut icon" href="/assets/img/glyphicons_054_clock.png" type="image/png">
-    <title><?= isset($title) ? $title . " - " : "" ?>Just Nyt!</title>
+    <title><?= isset($title) ? preg_replace("/\s+?(-|&mdash;) Just Nyt!$/i", "", $title) . " - " : "" ?>Just Nyt!</title>
 
-    <meta property="og:title" content="<?= isset($title) ? $title . " - " : "" ?>Just Nyt!">
-    <meta property="og:description"
-        content="Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone">
+    <meta property="og:title"
+        content="<?= isset($title) ? preg_replace("/\s+?(-|&mdash;) Just Nyt!$/i", "", $title) . " - " : "" ?>Just Nyt!">
+
+    <?php if (isset($description)): ?>
+        <meta property="og:description"
+            content="<?= $description ?>">
+    <?php else: ?>
+        <meta property="og:description"
+            content="Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone">
+    <?php endif; ?>
 </head>
 <body>
     <?php $this->snippet("snippets/header") ?>
