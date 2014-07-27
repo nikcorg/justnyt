257fbb1b91384889a5dfa368080684f8adc9ae91
Niklas Lindgren <niklas@sc5.io>
2014-07-27 13:55:54 +0300

    Remove redundant tag soup

--- justnyt/templates/snippets/footer.php
+++ justnyt/templates/snippets/footer.php
@@ -1,17 +1,13 @@
 <footer>
-    <div class="column left centered">
-        <nav>
-            <ul>
-                <li><a href="/parhautta" rel="nofollow">Parhautta</a></li>
-                <li><a href="/historiaa">Parhauden historiaa</a></li>
-                <li><a href="/kuraattorit">Kuraattorit</a></li>
-                <li><a href="/kuraattoriksi">Kuraattoriksi kuraattorin paikalle</a></li>
-                <li><a href="/faq">Kysymyksiä ja vastauksia</a></li>
-                <li><a href="/">Etusivulle</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div class="column left centered">
-        <p>Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone</p>
-    </div>
+    <nav class="centered">
+        <ul>
+            <li><a href="/parhautta" rel="nofollow">Parhautta</a></li>
+            <li><a href="/historiaa">Parhauden historiaa</a></li>
+            <li><a href="/kuraattorit">Kuraattorit</a></li>
+            <li><a href="/kuraattoriksi">Kuraattoriksi kuraattorin paikalle</a></li>
+            <li><a href="/faq">Kysymyksiä ja vastauksia</a></li>
+            <li><a href="/">Etusivulle</a></li>
+        </ul>
+    </nav>
+    <p class="centered">Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone</p>
 </footer>
