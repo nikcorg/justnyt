a5e129e26cdd2f52d07ce3bc87887bbdb9edbcbd
Niklas Lindgren <niklas@sc5.io>
2014-08-05 08:17:35 +0300

    Edit front page copy

--- justnyt/templates/pages/index.php
+++ justnyt/templates/pages/index.php
@@ -1,5 +1,7 @@
-<p>Just Nyt! on vapaaehtoisten kuraattoreiden valitseman sisällön suosittelukone.</p>
+<p>Just nyt auttaa sinua löytämään mielenkiintoista luettavaa.</p>
 
 <h2>Miten se toimii?</h2>
 
 <p>Tarvitset vain yhden kirjanmerkin selaimeesi: <a href="/parhautta">justnyt.fi/parhautta</a>. Se ohjaa sinut aina viimeisimpään suositukseen. Se, kuinka usein sisältö päivittyy, riippuu täysin kuraattorista.</p>
+
+<p>Suositeltujen historiaa voit selailla <a href="/historiaa">Historia-sivulta</a>. Aikaisemmat kuraattorit näet <a href="/kuraattorit">Kuraattorit-sivulta</a>.
