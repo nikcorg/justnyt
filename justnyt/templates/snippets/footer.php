c5555fecd2590afa85505c15c388a1cb0927835e
Niklas Lindgren <niklas@sc5.io>
2014-08-05 08:18:12 +0300

    Exclude faq from footer navi for now

--- justnyt/templates/snippets/footer.php
+++ justnyt/templates/snippets/footer.php
@@ -5,7 +5,7 @@
             <li><a href="/historiaa">Parhauden historiaa</a></li>
             <li><a href="/kuraattorit">Kuraattorit</a></li>
             <li><a href="/kuraattoriksi">Kuraattoriksi kuraattorin paikalle</a></li>
-            <li><a href="/faq">Kysymyksiä ja vastauksia</a></li>
+            <?php /* <li><a href="/faq">Kysymyksiä ja vastauksia</a></li> */ ?>
             <li><a href="/">Etusivulle</a></li>
         </ul>
     </nav>
