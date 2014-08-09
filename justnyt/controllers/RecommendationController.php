21f7ef0f956db2635462d91be5c2b41d4488c9bc
Niklas Lindgren <niklas@sc5.io>
2014-08-09 13:40:06 +0300

    Add routes and implementation for scrape queueing and execution

--- justnyt/controllers/RecommendationController.php
+++ justnyt/controllers/RecommendationController.php
@@ -3,15 +3,74 @@ namespace justnyt\controllers;
 
 class RecommendationController extends \glue\Controller
 {
-    public function create($token) {
+    protected function getCurator($token) {
         $curator = \justnyt\models\CuratorQuery::create()->getActiveCuratorByToken($token);
 
         if (is_null($curator)) {
             throw new \glue\exceptions\http\E403Exception();
         }
 
-        $url = $this->request->GET->url ?: "no url given";
+        return $curator;
+    }
+
+    public function scrape($token, $id) {
+        $curator = $this->getCurator($token);
+        $pending = \justnyt\models\RecommendationQuery::create()->findOneByRecommendationId($id);
+
+        if (! is_null($pending->getScrapedOn())) {
+            return $this->response->setJSONContent($pending->toJSON());
+        }
+
+        $curl = \glue\utils\Curl::factory($pending->getUrl());
+
+        if (! $curl->get() || $curl->getResponseCode() !== 200) {
+            throw new \glue\exceptions\http\E500Exception("Error scraping " . $pending->getUrl());
+        }
+
+        $dom = \Sunra\PhpSimple\HtmlDomParser::str_get_html($curl->getResponseBody());
+        $title = trim($dom->find("title", 0)->plaintext);
+
+        if (empty($title)) {
+            $title = "Untitled";
+        }
+
+        try {
+            $pending->setScrapedOn(time());
+            $pending->setTitle($title);
+            $pending->save();
+        } catch (\Exception $e) {
+            throw new \glue\exceptions\http\E500Exception("Error updating recommendation", 0, $e);
+        }
+
+        $this->response->setJSONContent($pending->toJSON());
+    }
+
+    public function prepare($token) {
+        $curator = $this->getCurator($token);
+        $url = $this->request->GET->url;
+
+        if (empty($url)) {
+            throw new \glue\exceptions\http\E400Exception("Empty URL");
+        }
+
+        try {
+            $prepare = new \justnyt\models\Recommendation();
+            $prepare->setCurator($curator);
+            $prepare->setCreatedOn(time());
+            $prepare->setUrl($this->request->GET->url);
+            $prepare->save();
+        } catch (\Exception $e) {
+            throw new \glue\exceptions\http\E500Exception("Error saving recommendation candidate", 0, $e);
+        }
+
+        // Add scrape job to queue, include job ID in response
         $this->response->setContentType("text/plain; charset=utf-8");
-        $this->response->setContent("Add url: " . ($url ?: "none"));
+        $this->response->setContent("Scrape job queued: " . ($prepare->getRecommendationId()));
+    }
+
+    public function approve($token) {
+        $curator = $this->getCurator($token);
+
+
     }
 }
