a1412aa02c4200e2a0a7fef4e5353681569d9d50
Niklas Lindgren <niklas@sc5.io>
2014-08-11 23:27:23 +0300

    Collapsed list

--- justnyt/templates/pages/curators.php
+++ justnyt/templates/pages/curators.php
@@ -1,6 +1,6 @@
 <p>Tästä listasta näet kuraattorit, jotka eivät halua esiintyä nimettöminä.</p>
 
-<ol reversed>
+<ol reversed class="collapsed">
 <?php foreach ($curators as $curator): ?>
     <?php $profile = $curator->getProfile(); ?>
     <li>
