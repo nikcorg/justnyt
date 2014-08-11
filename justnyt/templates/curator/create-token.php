bcfe619434ff0c0ad9530f0f81d64fff3517496d
Niklas Lindgren <niklas@sc5.io>
2014-08-12 00:41:22 +0300

    Send email to next volunteer in queue

--- justnyt/templates/curator/create-token.php
+++ justnyt/templates/curator/create-token.php
@@ -1,6 +1,17 @@
-<p>Lähetä alle oleva linkki seuraajallesi:</p>
+<p>Lähetä alle oleva linkki seuraajallesi, tai seuravalle jonossa olevalle vapaaehtoiselle.</p>
 
-<form>
+<form action="" method="post">
+    <div class="form-blocker hidden nomargin"></div>
+
     <p><label><span>Aktivointiosoite</span>
-        <input type="url" value="http://<?= $host ?>/kuraattori/<?= $token ?>/aktivoi"></label></p>
+        <input type="url" name="url" value="<?= $activationUrl ?>"></label></p>
+
+    <?php if ($mailSent): ?>
+        <p>Viesti lähetettiin seuraavalle vapaaehtoiselle.</p>
+    <?php elseif ($volunteers): ?>
+        <button type="submit" name="volunteer" value="0" class="hidden">Lähetän koodin itse</button>
+        <p><button type="submit" name="volunteer" value="1">Lähetä seuraavalle jonottajalle</button></p>
+    <?php else: ?>
+        <p>Valitettavasti ei tällä hetkellä ole yhtään vapaaehtoista jonossa.</p>
+    <?php endif; ?>
 </form>
---
+++ justnyt/templates/curator/email-body.php
@@ -0,0 +1,14 @@
+Hei,
+
+Sinulle on heitetty kuraattorinhattu. Ota se vastaan osoitteessa <?php $url ?>
+
+
+Terveisin
+
+JustNyt
+--
+http://justnyt.fi
+
+Olet saanut tämän sähköpostin koska sähköpostiosoitteesi on ilmoitettu
+JustNyt-palvelun vapaaehtoisten listalle. Mikäli osoitteesi on lisätty
+vahingossa, vastaa tähän sähköpostiin.
