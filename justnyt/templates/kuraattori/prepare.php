91fd40c5e1ed30123edc0cf270ab76116117f530
Niklas Lindgren <niklas@sc5.io>
2014-08-11 11:55:35 +0300

    Handle recommendation preview, scrape and activation

--- justnyt/templates/kuraattori/prepare.php
+++ justnyt/templates/kuraattori/prepare.php
@@ -1,4 +1,4 @@
-<form method="post" action="kuraattori/<?= $token ?>/approve/<?= $candidateId ?>">
+<form method="post" action="/kuraattori/<?= $token ?>/suosittelut/<?= $candidateId ?>" data-candidate-id="<?= $candidateId ?>" id="preview-form">
     <div class="form-blocker hidden nomargin"></div>
 
     <p>Tarkista tiedot ennen tallentamista. Suositus aktivoituu välittömästi tallentamisen jälkeen.</p>
@@ -7,7 +7,7 @@
         <input type="url" name="url" value="<?= $url ?>" required></label> (<a href="<?= $url ?>" target="_blank">Linkki</a>)</p>
 
     <p><label><span>Otsikko</span>
-        <input type="text" name="title" value="<?= $title ?>"></label></p>
+        <input type="text" name="title" value="<?= $title ?>" required></label></p>
 
     <p><button type="submit">Tallenna</button></p>
 </form>
