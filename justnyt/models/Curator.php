47d5b2451415033300e8f53562358c6c3722e1a1
Niklas Lindgren <niklas@sc5.io>
2014-08-11 22:58:09 +0300

    Generate unique token

--- justnyt/models/Curator.php
+++ justnyt/models/Curator.php
@@ -11,4 +11,30 @@ class Curator extends BaseCurator
 
         return $this->save();
     }
+
+    public function generateToken($tokenlen = 5) {
+        $candidates = 5;
+        $tokens = array();
+
+        for ($i = 0; $i < $candidates; $i++) {
+            $hash = sha1(time() . rand());
+            array_push($tokens, substr($hash, rand(0, strlen($hash) - $tokenlen - 1), $tokenlen));
+        }
+
+        $unusables = \justnyt\models\CuratorQuery::create("cr")
+            ->select("Token")
+            ->where("cr.Token IN ('" . implode("','", $tokens) . "')")
+            ->find()
+            ->toArray();
+
+        $tokens = array_diff($tokens, $unusables);
+
+        // If all collide, increase token length
+        // TODO: add alert for this event
+        if (count($tokens) === 0) {
+            return $this->generateToken($tokenlen + 1);
+        }
+
+        $this->setToken(array_pop($tokens));
+    }
 }
