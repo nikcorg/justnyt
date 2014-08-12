089fd7cdbb09c77f40a186b21a6afc99c4dde365
Niklas Lindgren <niklas@sc5.io>
2014-08-12 08:42:52 +0300

    Generate shortlink hashes

--- justnyt/models/Recommendation.php
+++ justnyt/models/Recommendation.php
@@ -6,5 +6,25 @@ use justnyt\models\Base\Recommendation as BaseRecommendation;
 
 class Recommendation extends BaseRecommendation
 {
+    public function generateShortLink($tokenlen = 3) {
+        $candidates = 5;
+        $tokens = array_filter(str_split(sha1($this->url), $tokenlen), function ($val) use ($tokenlen) {
+            return strlen($val) == $tokenlen;
+        });
+        $unusables = \justnyt\models\RecommendationQuery::create("rd")
+            ->select("Shortlink")
+            ->where("rd.Shortlink IN ('" . implode("','", $tokens) . "')")
+            ->find()
+            ->toArray();
 
+        $tokens = array_diff($tokens, $unusables);
+
+        // If all collide, increase token length
+        // TODO: add alert for this event
+        if (count($tokens) === 0) {
+            return $this->generateShortLink($tokenlen + 1);
+        }
+
+        $this->setShortlink(array_shift($tokens));
+    }
 }
