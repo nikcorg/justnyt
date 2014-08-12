bb8245a8203dd888811a8fcd26851e6f6bc43842
Niklas Lindgren <niklas@sc5.io>
2014-08-12 12:22:04 +0300

    Add missing xml namespace

--- justnyt/templates/feed/rss.php
+++ justnyt/templates/feed/rss.php
@@ -1,5 +1,8 @@
 <?php print("<?"); ?>xml version="1.0" encoding="UTF-8"<?php print("?>\n"); ?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+<rss version="2.0"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/"
+    xmlns:atom="http://www.w3.org/2005/Atom"
+>
 <channel>
     <title>JustNyt!</title>
     <link>http://justnyt.fi</link>
