111148fe98fb0b91448e5c033b2105aa985f384f
Niklas Lindgren <niklas@sc5.io>
2014-08-12 08:58:11 +0300

    Tweak shortlink hash generation algorithm

--- justnyt/models/Recommendation.php
+++ justnyt/models/Recommendation.php
@@ -8,16 +8,18 @@ class Recommendation extends BaseRecommendation
 {
     public function generateShortLink($tokenlen = 3) {
         $candidates = 5;
-        $tokens = array_filter(str_split(sha1($this->url), $tokenlen), function ($val) use ($tokenlen) {
-            return strlen($val) == $tokenlen;
-        });
+        $hash = sha1($this->getUrl() . microtime(true) . rand());
+        $matches = array();
+
+        preg_match_all("/([a-z].{" . ($tokenlen - 1) . "})/", $hash, $matches, PREG_PATTERN_ORDER);
+
         $unusables = \justnyt\models\RecommendationQuery::create("rd")
             ->select("Shortlink")
-            ->where("rd.Shortlink IN ('" . implode("','", $tokens) . "')")
+            ->where("rd.Shortlink IN ('" . implode("','", $matches[0]) . "')")
             ->find()
             ->toArray();
 
-        $tokens = array_diff($tokens, $unusables);
+        $tokens = array_diff($matches[0], $unusables);
 
         // If all collide, increase token length
         // TODO: add alert for this event
@@ -26,5 +28,13 @@ class Recommendation extends BaseRecommendation
         }
 
         $this->setShortlink(array_shift($tokens));
+    }
+
+    public function save(\Propel\Runtime\Connection\ConnectionInterface $con = null) {
+        if (is_null($this->getShortlink())) {
+            $this->generateShortLink();
+        }
+
+        return parent::save($con);
     }
 }
