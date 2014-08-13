5ac02869b2cedb112e51d9fa3cd1ee920c672b7a
Niklas Lindgren <niklas@sc5.io>
2014-08-13 13:43:38 +0300

    Text changes

--- justnyt/templates/curator/home.php
+++ justnyt/templates/curator/home.php
@@ -2,14 +2,14 @@
 
 <p><a href="/kuraattori/<?= $curator->getToken() ?>/profiili">Muokkaa profiiliasi</a>, jos haluat. Se on täysin vapaaehtoista.</p>
 
-<h2>Miten suosittelen</h2>
+<h2>Miten suosittelet</h2>
 <p>Kaikki mitä tarvitset on yllä oleva kirjanmerkki. Kun luet jotain mitä haluat suositella, paina kirjanmerkkiä. Älä huoli, sinulta varmistetaan tietojen oikeellisuus.</p>
 <p>Valitettavasti mobiilisuosittelu on vielä työn alla. Voit tietysti käyttää samaa mekanismia myös mobiilissa kuin pöytäkoneellasi, mutta se on ärsyttävän työlästä. Toimiva, helppo ratkaisu on etsinnässä.</p>
 
-<h2>Mitä suosittelisin</h2>
-<p>Mitä tahansa, mikä sinusta on suosittelun arvoista, johon voi linkata suoraan. Suosi kuitenkin ensisijaisesti blogisisältöä ja mielellään suomenkielistä. Tärkeintä on, että suositus vie suoraan sisältöön, eikä esimerkiksi etusivulle.</p>
+<h2>Mitä suosittelisit</h2>
+<p>Suosittele mitä tahansa sinusta suosittelun arvoista, johon voi linkata suoraan. Suosi kuitenkin ensisijaisesti blogisisältöä ja mielellään suomenkielistä. Tärkein kriteeri on kuitenkin se, että suositus vie suoraan sisältöön, eikä esimerkiksi etusivulle.</p>
 
 <h2>Kuinka päätän kuraattorikauteni</h2>
 <p>Kuraattorikautesi pituuden päätät sinä itse. Kun et enää haluat pitää kuraattorinhattua, voit jakaa sen seuraavalle kahdella tapaa: voit antaa sen suoraan jollekulle, tai poimia seuraavan vapaaehtoisen jonosta.</p>
 <p>Oma kautesi päättyy kun seuraajasi aktivoi oman kuraattorikautensa. Tämän jälkeen tunnuksesi ei enää toimi, etkä voi tehdä uusia suosituksia, tai muokata profiiliasi.</p>
-<p>Eli, sitten kun hattu ei enää sovi päähän, <a href="/kuraattori/<?= $curator->getToken() ?>/seuraava">kutsu seuraava kuraattori</a>.</p>
+<p>Toisin sanoen, kun hattu ei enää istu omaan päähän, on aika <a href="/kuraattori/<?= $curator->getToken() ?>/seuraava">kutsua seuraava kuraattori</a>.</p>
