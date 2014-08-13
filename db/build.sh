d7fdc6cb42727bff9b0153e4b6ea131eebce3324
Niklas Lindgren <niklas@sc5.io>
2014-08-13 18:12:51 +0300

    Alter schema on successful reverse only

--- db/build.sh
+++ db/build.sh
@@ -1,8 +1,11 @@
 PROPEL="../vendor/bin/propel"
 
-$PROPEL reverse "mysql:host=localhost;dbname=justnyt;user=justnyt;password=justnyt" --output-dir . --database-name justnyt
-
-cat schema.xml|sed -e 's/<table\(.*\)phpName="/<table\1namespace="justnyt\\models" phpName="/g'>schema.tmp&&mv schema.tmp schema.xml
+$PROPEL reverse "mysql:host=localhost;dbname=justnyt;user=justnyt;password=justnyt" \
+    --output-dir . \
+    --database-name justnyt && \
+    cat schema.xml | \
+    sed -e 's/<table\(.*\)phpName="/<table\1namespace="justnyt\\models" phpName="/g'> schema.tmp && \
+    mv schema.tmp schema.xml
 
 $PROPEL convert-conf
 $PROPEL model:build --output-dir ..
