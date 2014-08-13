c64206ad7c7a576565e8b8c991c0cb33602fe9db
Niklas Lindgren <niklas@sc5.io>
2014-08-13 19:02:26 +0300

    Move webfont loader to global.js

--- client/global.js
+++ client/global.js
@@ -6,3 +6,18 @@ function createEl(el) {
 }
 
 for (i in elems) createEl(elems[i]);
+
+/* Webfonts */
+
+var WebFontConfig = window.WebFontConfig = {
+    google: { families: [ 'Special+Elite::latin' ] }
+};
+(function() {
+var wf = document.createElement('script');
+wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
--- justnyt/templates/layout.php
+++ justnyt/templates/layout.php
@@ -36,21 +36,6 @@
 
     <?php $this->snippet("snippets/footer") ?>
 
-    <script type="text/javascript">
-        WebFontConfig = {
-            google: { families: [ 'Special+Elite::latin' ] } //, 'Luckiest+Guy::latin' ] }
-        };
-        (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-        })();
-    </script>
-
     <?php if (isset($scripts) && is_array($scripts)): ?>
         <?php foreach ($scripts as $src): ?>
             <script type="text/javascript" src="<?= $src ?>" async defer></script>
