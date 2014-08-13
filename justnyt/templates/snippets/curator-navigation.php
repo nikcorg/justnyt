d9b203624433e05dc374a63126a4dbdbbb963599
Niklas Lindgren <niklas@sc5.io>
2014-08-13 11:19:57 +0300

    Add link to pending queue, with queue length blurb

--- justnyt/templates/snippets/curator-navigation.php
+++ justnyt/templates/snippets/curator-navigation.php
@@ -3,5 +3,8 @@
     <li><a href="/kuraattori/<?= $token ?>/tervetuloa">Tervetuloa</a></li>
     <li><a href="/kuraattori/<?= $token ?>/profiili">Muokkaa profiiliasi</a></li>
     <li><a href="/kuraattori/<?= $token ?>/seuraava">Kutsu seuraava kuraattori</a></li>
+    <?php if (isset($pending)): ?>
+        <li><a href="/kuraattori/<?= $token ?>/jonossa">Julkaisujono <span class="blurb"><?= $pending ?></span></a></li>
+    <?php endif; ?>
 </ul>
 </nav>
