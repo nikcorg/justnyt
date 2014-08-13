323989ece6e45ae1a7618a0b47f0def991c1ae99
Niklas Lindgren <niklas@sc5.io>
2014-08-13 18:50:57 +0300

    Hide pending queue when empty

--- justnyt/templates/snippets/curator-navigation.php
+++ justnyt/templates/snippets/curator-navigation.php
@@ -3,7 +3,7 @@
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/tervetuloa">Tervetuloa</a></li>
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/profiili">Muokkaa profiiliasi</a></li>
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/seuraava">Kutsu seuraava kuraattori</a></li>
-    <?php if (isset($pending)): ?>
+    <?php if (isset($pending) && $pending > 0): ?>
         <li><a href="/kuraattori/<?= $curator->getToken() ?>/jonossa">Julkaisujono <span class="blurb"><?= $pending ?></span></a></li>
     <?php endif; ?>
 </ul>
