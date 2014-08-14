397c52135b118fa2b28b5e8389d5094cfeaf9da2
Niklas Lindgren <niklas@sc5.io>
2014-08-15 00:29:10 +0300

    Alter email info text

--- justnyt/templates/pages/email-info.php
+++ justnyt/templates/pages/email-info.php
@@ -1,5 +1,4 @@
-<p>Ole huoleti, emme luovuta sähköpostiosoitettasi kolmansille osapuolille. Emme myöskään itse postita siihen mitään, paitsi jos meillä on siihen erityisen hyvä syy.</p>
-<p>Jos täytät profiilitietoihisi sähköpostiosoitteen, voimme lähettää sinulle tiedon, kun seuraava kuraattori aktivoi oman tilinsä.</p>
+<p>Ole huoleti, sähköpostiosoitettasi ei näytetä sivustolla, eikä luovuteta kolmansille osapuolille. Siihen ei myöskään postiteta mitään, pois lukien järjestelmän automaattiset tiedonannot. Niistä lisää alla.</p>
+<p>Jos täytät profiilitietoihisi sähköpostiosoitteen, saat sähköpostiisi tiedon, kun seuraava kuraattori aktivoi oman tilinsä.</p>
 <p>Mikäli sähköpostisi on yhdistetty <a href="https://fi.gravatar.com">Gravatar</a>-tiliin, näytetään kuva julkisessa kuraattorinprofiilissasi.</p>
-<p>Sähköpostiosoitteen avulla voimme tunnistaa eri kuraattorikautesi, jolloin ne näkyvät kaikki profiilissasi.</p>
-<p>Saatamme myös muistuttaa sinua kuraattorintehtävistäsi, mikäli suositustesi välillä on kovin pitkä aika.</p>
+<p>Sähköpostiosoitteen avulla voimme tunnistaa eri kuraattorikautesi, jolloin ne näkyvät kaikki profiilissasi allekkain.</p>
