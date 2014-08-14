72078f113af3ce7d688a46bf183bb2a75d1d4f2c
Niklas Lindgren <niklas@sc5.io>
2014-08-15 01:03:04 +0300

    Add profile controller

---
+++ justnyt/controllers/ProfileController.php
@@ -0,0 +1,52 @@
+<?php
+namespace justnyt\controllers;
+
+class ProfileController extends \glue\Controller
+{
+    public function profile($curatorId, $profileId, $alias) {
+        $curator = \justnyt\models\CuratorQuery::create("cr")
+            ->joinWith("Profile")
+            ->joinProfile("pr")
+            ->where("pr.Alias IS NOT NULL")
+            ->where("pr.Alias != ''")
+            ->where("cr.CuratorId = ?", $curatorId)
+            ->findOne();
+
+        if (is_null($curator)) {
+            throw new \glue\exceptions\http\E404Exception();
+        }
+
+        $profile = $curator->getProfile();
+
+        if (! is_null($profile->getEmail())) {
+            $recommendations = null;
+            $profiles = \justnyt\models\ProfileQuery::create("pr")
+                ->joinWith("Profile.Curator")
+                ->joinWith("Curator.Recommendation")
+                ->useCuratorQuery("cr")
+                    ->useRecommendationQuery("re")
+                        ->endUse()
+                    ->endUse()
+                ->where("pr.Email = ?", $profile->getEmail())
+                ->orderBy("cr.ActivatedOn", "DESC")
+                ->orderBy("re.ApprovedOn", "ASC")
+                ->find();
+        } else {
+            $profiles = null;
+            $recommendations = $curator->getRecommendations();
+        }
+
+        $this->response->setContentType("text/html; charset=utf-8");
+        $this->response->setContent(
+            \justnyt\views\JustNytLayout::quickRender(
+                "profile/profile",
+                array(
+                    "title" => $profile->getAlias(),
+                    "profile" => $profile,
+                    "recommendations" => $recommendations,
+                    "profiles" => $profiles
+                    )
+                )
+        );
+    }
+}
