8d512d551063f8c71ca216cdc1fcefedbc988dc9
Niklas Lindgren <niklas@sc5.io>
2014-08-17 20:08:35 +0300

    Record all visits

--- justnyt/controllers/RedirectController.php
+++ justnyt/controllers/RedirectController.php
@@ -3,8 +3,24 @@ namespace justnyt\controllers;
 
 class RedirectController extends \glue\Controller
 {
+    protected function createVisit() {
+        $visitorId = $this->request->COOKIES->vid;
+
+        if (null == $visitorId) {
+            $visitorId = substr(uniqid(rand() . ".", true), 0, 32);
+        }
+
+        // Update the cookie lifetime
+        setcookie("vid", $visitorId, time() + 2592000 /* 30 days */, "/", $_SERVER["HTTP_HOST"], false, true);
+
+        $visit = new \justnyt\models\Visit();
+        $visit->setVisitorId($visitorId)->setRecordedOn($_SERVER["REQUEST_TIME"]);
+
+        return $visit;
+    }
+
     protected function redirectTo($recommendation) {
-        $recommendation->addVisit();
+        $recommendation->addVisit($this->createVisit())->save();
 
         $url = $recommendation->getUrl();
         $url .= (strpos($url, "?") === false ? "?" : "&") . "utm_source=justnyt.fi";
