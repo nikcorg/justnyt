1a549e897905adbf7052bceaffc89c7fc596c99d
Niklas Lindgren <niklas@sc5.io>
2014-08-18 21:07:02 +0300

    Add deactivation email template

---
+++ justnyt/templates/email/account-deactivated.php
@@ -0,0 +1,21 @@
+Hei,
+
+Seuraava kuraattori on aktivoinut tilins ja kuraattorikautesi on nyt päättynyt.
+Jätä nimesi jonoon, kun kaipaat kuraattorinhattua jälleen päätäsi koristamaan.
+
+Alla vielä julkaisemasi suositukset kävijämäärineen.
+
+<?php while ($recommendation = $approved->fetch(\PDO::FETCH_OBJ)): ?>
+    <?= $recommendation->TITLE ?>
+    <?= $recommendation->URL ?>
+    <?= $recommendation->CLICKS ?> klikkausta <?= $recommendation->VISITORS ?> kävijältä
+<?php endwhile; ?>
+
+Kiitos ajastasi.
+
+
+Terveisin
+
+JustNyt
+--
+http://justnyt.fi
