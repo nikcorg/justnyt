33b2e73b31cb8d869245fc69070e9c2a5e3875f6
Niklas Lindgren <nikc@iki.fi>
2014-08-22 16:26:40 +0300

    Add link to facebook page

--- justnyt/templates/pages/index.php
+++ justnyt/templates/pages/index.php
@@ -10,4 +10,4 @@
 
 <p>Mikäli mielesi tekee kuraattorinhattua, ilmoittaudu <a href="/kuraattoriksi">vapaaehtoiseksi</a>.</p>
 
-<p>Voit myös seurata suosituksia <a href="http://twitter.com/justnytfi">Twitterissä</a></p>
+<p>Voit myös seurata suosituksia <a href="http://twitter.com/justnytfi">Twitterissä</a> ja <a href="https://www.facebook.com/justnytfi">Facebookissa</a>.</p>
