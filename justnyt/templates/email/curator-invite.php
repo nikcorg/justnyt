ff8fa11dd96ff3a1294a711e5deb25fa436478bc
Niklas Lindgren <nikc@iki.fi>
2014-08-22 14:51:00 +0300

    Add note suggesting the email is stored for the curation period

--- justnyt/templates/email/curator-invite.php
+++ justnyt/templates/email/curator-invite.php
@@ -6,9 +6,12 @@ Sinulle on heitetty kuraattorinhattu. Ota se vastaan osoitteessa <?= $url ?>
 Terveisin
 
 JustNyt
+
+
+PS. Säilytä tämä sähköposti koko kuraattorikauden ajan, niin tunnuksesi ei mene vahingossa hukkaan.
 --
 http://justnyt.fi
 
 Olet saanut tämän sähköpostin koska sähköpostiosoitteesi on ilmoitettu
-JustNyt-palvelun vapaaehtoisten listalle. Mikäli osoitteesi on lisätty
-vahingossa, vastaa tähän sähköpostiin.
+JustNyt-palvelun vapaaehtoisten kuraattoreiden listalle. Mikäli osoitteesi
+on lisätty vahingossa, vastaa tähän sähköpostiin.
