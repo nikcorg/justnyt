1801dd3e432e377ac0b0272d577aa336a0ab20f8
Niklas Lindgren <nikc@iki.fi>
2014-08-23 13:03:39 +0300

    Add required property to url input

--- justnyt/templates/recommendation/create.php
+++ justnyt/templates/recommendation/create.php
@@ -1,5 +1,5 @@
 <form method="get" action="/kuraattori/<?= $curator->getToken() ?>/esikatsele">
     <p><label><span>URL-osoite</span>
-        <input type="url" name="url" value="" placeholder="suositeltavan sisällön osoite tähän"/></label></p>
+        <input type="url" name="url" value="" placeholder="suositeltavan sisällön osoite tähän" required/></label></p>
     <p><button type="submit">Suosittele</button></p>
 </form>
