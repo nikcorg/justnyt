b114c427fc33f54b1c5805414640c02c9e626f33
Niklas Lindgren <nikc@iki.fi>
2014-08-26 16:36:26 +0300

    Add feedback request to index page

--- justnyt/templates/pages/index.php
+++ justnyt/templates/pages/index.php
@@ -16,3 +16,5 @@
 <p>Mikäli mielesi tekee kuraattorinhattua, ilmoittaudu <a href="/kuraattoriksi">vapaaehtoiseksi</a>.</p>
 
 <p>Voit myös seurata suosituksia <a href="http://twitter.com/justnytfi">Twitterissä</a>, <a href="https://www.facebook.com/justnytfi">Facebookissa</a> ja <a href="https://plus.google.com/103263858445378300353/posts">Google Plussassa</a>.</p>
+
+<p>Palautetta ja kehitysehdotuksia otetaan mieluusti vastaan kaikissa kanavissa.</p>
