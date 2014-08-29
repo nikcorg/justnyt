146c730ed62a470452cda3359629cded365d6d4d
Niklas Lindgren <nikc@iki.fi>
2014-08-29 18:16:32 +0300

    Add simple maintenance mode

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+if (getenv("MAINTENANCE")) {
+    header("Content-Type: text/html; charset=utf-8", true, 503);
+    readfile(dirname(__FILE__) . "/maintenance.html");
+    die();
+}
+
 define("GLUE_APPHOME", dirname(__DIR__));
 define("GLUE_CONFIG", GLUE_APPHOME . "/config.php");
 define("GLUE_RUNNER", "\\justnyt\\JustNytApp");
