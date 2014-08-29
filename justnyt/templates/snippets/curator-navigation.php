a613b3acbc63679d4e3bddf3c7e70ef4234c52ac
Niklas Lindgren <nikc@iki.fi>
2014-08-29 18:43:08 +0300

    Add link to hints overview tool

--- justnyt/templates/snippets/curator-navigation.php
+++ justnyt/templates/snippets/curator-navigation.php
@@ -3,6 +3,7 @@
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/tervetuloa">Tervetuloa</a></li>
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/profiili">Muokkaa profiiliasi</a></li>
     <li><a href="/kuraattori/<?= $curator->getToken() ?>/seuraava">Kutsu seuraava kuraattori</a></li>
+    <li><a href="/kuraattori/<?= $curator->getToken() ?>/vinkatut">Ilmiannot</a></li>
     <?php if (isset($pending) && $pending > 0): ?>
         <li><a href="/kuraattori/<?= $curator->getToken() ?>/jonossa">Julkaisujono <span class="blurb"><?= $pending ?></span></a></li>
     <?php endif; ?>
