90a8d2b07f3550dd17251c5a9f0a42dcad9b312c
Niklas Lindgren <nikc@iki.fi>
2014-11-25 15:39:30 +0200

    Add dc:creator and author fields
    Author field contents is not according to spec (should be en e-mail)
    but has to be this way for it to be usable in IFTTT.

--- justnyt/templates/feed/rss.php
+++ justnyt/templates/feed/rss.php
@@ -2,6 +2,7 @@
 <rss version="2.0"
     xmlns:content="http://purl.org/rss/1.0/modules/content/"
     xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
 >
 <channel>
     <title>JustNyt!</title>
@@ -13,7 +14,9 @@
             <guid isPermaLink="true">http://<?= $host ?>/s/<?= $item->getShortLink() ?></guid>
             <pubDate><?= $item->getApprovedOn()->setTimezone(new DateTimeZone("UTC"))->format("D, d M Y H:i:s O") ?></pubDate>
             <title><?= $item->getTitle() ?></title>
+            <dc:creator><?php if (null == $item->getRecommendationHint()): ?>Kuraattori<?php else: ?>Ilmiantaja<?php endif; ?></dc:creator>
             <atom:author><?php if (null == $item->getRecommendationHint()): ?>Kuraattori<?php else: ?>Ilmiantaja<?php endif; ?></atom:author>
+            <author><?php if (null == $item->getRecommendationHint()): ?>Kuraattori<?php else: ?>Ilmiantaja<?php endif; ?></author>
             <link>http://<?= $host ?>/s/<?= $item->getShortLink() ?></link>
             <description><![CDATA[<?php if (null != $item->getQuote() && "" != $item->getQuote()): ?><blockquote><?= $item->getQuote() ?></blockquote><?php endif; ?>]]></description>
             <content:encoded><![CDATA[<?php if (null != $item->getQuote() && "" != $item->getQuote()): ?><blockquote><?= $item->getQuote() ?></blockquote><?php endif; ?>]]></content:encoded>
