8071f9e081086488c95233bd2fe04720c52222f1
Niklas Lindgren <nikc@iki.fi>
2015-01-13 16:47:10 +0200

    Add HSTS header to all responses

--- justnyt/JustNytApp.php
+++ justnyt/JustNytApp.php
@@ -3,5 +3,9 @@ namespace justnyt;
 
 class JustNytApp extends \glue\Runnable
 {
+    protected function configure() {
+        \glue\net\Response::setGlobalHeader("Strict-Transport-Security", "max-age=14515200; includeSubDomains");
 
+        return parent::configure();
+    }
 }
