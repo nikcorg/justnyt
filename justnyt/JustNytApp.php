756556999738ea313520355eab8b280ad1ee66db
Niklas Lindgren <nikc@iki.fi>
2015-01-14 18:28:24 +0200

    Add X-Frame-Options header to all responses

--- justnyt/JustNytApp.php
+++ justnyt/JustNytApp.php
@@ -5,6 +5,7 @@ class JustNytApp extends \glue\Runnable
 {
     protected function configure() {
         \glue\net\Response::setGlobalHeader("Strict-Transport-Security", "max-age=14515200; includeSubDomains");
+        \glue\net\Response::setGlobalHeader("X-Frame-Options", "DENY");
 
         return parent::configure();
     }
