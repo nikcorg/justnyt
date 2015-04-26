0f56db6a7c79cb4c6039d66c2b681a747617a83e
Niklas Lindgren <nikc@iki.fi>
2015-04-26 14:26:34 +0300

    Open graph tweaks

--- justnyt/templates/layout.php
+++ justnyt/templates/layout.php
@@ -8,6 +8,11 @@
     <title><?= isset($title) ? preg_replace("/\s+?(-|&mdash;) Just Nyt!$/i", "", $title) . " - " : "" ?>Just Nyt!</title>
     <meta name="google-site-verification" content="EpMiQogg3lp02uS0ozMODwsMHemGQgE7F10LZm0wDUc" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+    <meta property="og:url" content="https://justnyt.fi"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:locale" content="fi_FI"/>
+    <meta property="og:site_name" content="Just Nyt!"/>
+    <meta property="og:image" content="https://justnyt.fi/assets/img/justnytfi.jpg"/>
     <meta property="og:title"
         content="<?= isset($title) ? preg_replace("/\s+?(-|&mdash;) Just Nyt!$/i", "", $title) . " - " : "" ?>Just Nyt!">
 
